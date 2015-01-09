e107d3ebb558ab29cf823e88f443a99ec5531351
Chase Southwood <chase.southwood@gmail.com>
2015-01-09 13:27:41 -0600

    Clean up some impls and macro-ify.

--- src/impls.rs
+++ src/impls.rs
@@ -1,66 +1,63 @@
-use std::collections::{HashMap, BTreeMap, VecMap, HashSet, BTreeSet};
+use std::collections::BTreeSet;
 
-use super::traits::{SortedMap, SortedSet};
+use super::traits::SortedSet;
 
-impl<T: Ord + Clone> SortedSet<T> for BTreeSet<T> {
-    fn first(&mut self, remove: bool) -> Option<T> {
-        if self.is_empty() { return None }
+macro_rules! sortedset_impl {
+    ($typ:ty) => (
+        impl<T: Ord + Clone> SortedSet<T> for $typ {
+            fn first(&mut self, remove: bool) -> Option<T> {
+                if self.is_empty() { return None }
 
-        let mut vec: Vec<T> = self.iter().cloned().collect();
-        let ret = vec.remove(0);
-        if remove {
-            self.remove(&ret);
+                let ret = self.iter().min().unwrap().clone();
+                if remove {
+                    self.remove(&ret);
+                }
+                Some(ret)
+            }
+
+            fn last(&mut self, remove: bool) -> Option<T> {
+                if self.is_empty() { return None }
+
+                let ret = self.iter().max().unwrap().clone();
+                if remove {
+                    self.remove(&ret);
+                }
+                Some(ret)
+            }
+
+            fn head_set(&self, elem: &T, inclusive: bool) -> $typ {
+                if inclusive {
+                    return self.iter().cloned().filter(|x| x <= elem).collect();
+                } else {
+                    return self.iter().cloned().filter(|x|  x < elem).collect();
+                }
+            }
+
+            fn sub_set(&self, from_elem: &T, from_inclusive: bool, to_elem: &T, to_inclusive:bool) -> $typ {
+                assert!(from_elem <= to_elem);
+
+                if from_inclusive && to_inclusive {
+                    return self.iter().cloned().filter(|x| x >= from_elem && x <= to_elem).collect();
+                } else if from_inclusive && !to_inclusive {
+                    return self.iter().cloned().filter(|x| x >= from_elem && x < to_elem).collect();
+                } else if !from_inclusive && to_inclusive {
+                    return self.iter().cloned().filter(|x| x > from_elem && x <= to_elem).collect();
+                } else {
+                    return self.iter().cloned().filter(|x| x > from_elem && x < to_elem).collect();
+                }
+            }
+
+            fn tail_set(&self, elem: &T, inclusive: bool) -> $typ {
+                if inclusive {
+                    return self.iter().cloned().filter(|x| x >= elem).collect();
+                } else {
+                    return self.iter().cloned().filter(|x| x > elem).collect();
+                }
+            }
         }
-        Some(ret)
-    }
-
-    fn last(&mut self, remove: bool) -> Option<T> {
-        if self.is_empty() { return None }
-
-        let mut vec: Vec<T> = self.iter().rev().cloned().collect();
-        let ret = vec.remove(0);
-        if remove {
-            self.remove(&ret);
-        }
-        Some(ret)
-    }
-
-    fn head_set(&self, elem: &T, inclusive: bool) -> BTreeSet<T> {
-        let mut vec: Vec<T>;
-        if inclusive {
-            vec = self.iter().cloned().filter(|x| x <= elem).collect();
-        } else {
-            vec = self.iter().cloned().filter(|x|  x < elem).collect();
-        }
-        vec.into_iter().collect::<BTreeSet<T>>()
-    }
-
-    fn sub_set(&self, from_elem: &T, from_inclusive: bool, to_elem: &T, to_inclusive:bool) -> BTreeSet<T> {
-        assert!(from_elem <= to_elem);
-
-        let mut vec: Vec<T>;
-        if from_inclusive && to_inclusive {
-            vec = self.iter().cloned().filter(|x| x >= from_elem && x <= to_elem).collect();
-        } else if from_inclusive && !to_inclusive {
-            vec = self.iter().cloned().filter(|x| x >= from_elem && x < to_elem).collect();
-        } else if !from_inclusive && to_inclusive {
-            vec = self.iter().cloned().filter(|x| x > from_elem && x <= to_elem).collect();
-        } else {
-            vec = self.iter().cloned().filter(|x| x > from_elem && x < to_elem).collect();
-        }
-        vec.into_iter().collect::<BTreeSet<T>>()
-    }
-
-    fn tail_set(&self, elem: &T, inclusive: bool) -> BTreeSet<T> {
-        let mut vec: Vec<T>;
-        if inclusive {
-            vec = self.iter().cloned().filter(|x| x >= elem).collect();
-        } else {
-            vec = self.iter().cloned().filter(|x| x > elem).collect();
-        }
-        vec.into_iter().collect::<BTreeSet<T>>()
-    }
+    );
 }
+sortedset_impl!(BTreeSet<T>);
 
 #[cfg(test)]
 mod tests {
