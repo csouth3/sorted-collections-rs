c70daacbb7f12bc04a6156c38ee5c0f1dee6ed40
Chase Southwood <chase.southwood@gmail.com>
2015-01-12 15:21:45 -0600

    Get ready for first release

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+//! sorted-collections-rs is a library providing useful extension traits and convenience
+//! methods for ordered collections in Rust.
+
+#![experimental]
+
 #![allow(unstable)]
 
 #[cfg(test)] extern crate test;
 
-pub use sortedmap::SortedMap;
-pub use sortedset::SortedSet;
+#[experimental] pub use sortedmap::SortedMap;
+#[experimental] pub use sortedset::SortedSet;
 
-pub mod sortedmap;
-pub mod sortedset;
+#[experimental] pub mod sortedmap;
+#[experimental ]pub mod sortedset;
