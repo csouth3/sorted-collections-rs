c727db76a533dc547d0410ebed51cf440247d48c
Chase Southwood <chase.southwood@gmail.com>
2015-01-16 14:25:20 -0600

    Deploy docs

---
+++ implementors/core/iter/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['sorted-collections'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='sorted-collections/sortedmap/struct.BTreeMapRangeRemove.html' title='sorted-collections::sortedmap::BTreeMapRangeRemove'>BTreeMapRangeRemove</a>&lt;K, V&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.DoubleEndedIterator.html' title='core::iter::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='sorted-collections/sortedset/struct.BTreeSetRangeRemove.html' title='sorted-collections::sortedset::BTreeSetRangeRemove'>BTreeSetRangeRemove</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['sorted-collections'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.ExactSizeIterator.html' title='core::iter::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='sorted-collections/sortedmap/struct.BTreeMapRangeRemove.html' title='sorted-collections::sortedmap::BTreeMapRangeRemove'>BTreeMapRangeRemove</a>&lt;K, V&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.ExactSizeIterator.html' title='core::iter::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='sorted-collections/sortedmap/struct.HashMapRangeRemove.html' title='sorted-collections::sortedmap::HashMapRangeRemove'>HashMapRangeRemove</a>&lt;K, V&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.ExactSizeIterator.html' title='core::iter::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='sorted-collections/sortedset/struct.BTreeSetRangeRemove.html' title='sorted-collections::sortedset::BTreeSetRangeRemove'>BTreeSetRangeRemove</a>&lt;T&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.ExactSizeIterator.html' title='core::iter::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='sorted-collections/sortedset/struct.HashSetRangeRemove.html' title='sorted-collections::sortedset::HashSetRangeRemove'>HashSetRangeRemove</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
